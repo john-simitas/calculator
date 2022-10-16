c67129d92c769f190cb0c4521b1e20105169f271
John Simtas <johnsimitas06@gmail.com>
2022-10-16 22:16:17 +0300

    Update scnd_grade file

--- src/scnd_grade.py
+++ src/scnd_grade.py
@@ -1,10 +1,2 @@
 def scnd_grade(a,b,c):
-    if a == str(a):
-        d = b ** 2 - 4 * a * c
-        print(d)
-    elif b == b ** 2:
-        d = b ** 2 - 4 * a * c
-        print(d)
-    elif c == c ** 2:
-        d = b ** 2 - 4 * a * c
-        print(d)
+    d = b ** 2 - 4 * a * c
